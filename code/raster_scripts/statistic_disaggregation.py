6cd194a6918ffde371630f3141ccebe19b4b33d6
Joe Molloy <joe.m34@gmail.com>
2016-05-27 12:21:47 +0200

    script to add land_use areas to regions

--- code/raster_scripts/statistic_disaggregation.py
+++ code/raster_scripts/statistic_disaggregation.py
@@ -10,10 +10,13 @@
         get the percentage coverage of each land use type
         based on inputed parameters
 '''
-from shapely.geometry import box, shape
-from rtree import index
-import fiona
 
+import fiona
+import rasterstats
+import ConfigParser
+import sys, os
+from itertools import repeat
+from shapely.geometry import shape
 
 #CASE WHEN OBJART  = 'AX_Wohnbauflaeche' THEN 1
 #	WHEN OBJART  =  'AX_FlaecheBesondererFunktionalerPraegung' THEN 2
@@ -28,9 +31,76 @@ values = ["remainder",
  'AX_IndustrieUndGewerbeflaeche',
  'AX_SportFreizeitUndErholungsflaeche']
 
-def calculate_region_land_use(regions, land_use_raster, land_use_categories):
-    pass
+#land_use_categories: (band, name)
 
+def calculate_region_land_use(region_shapefile, output_shapefile, land_use_raster_single_band, land_use_categories):
+    with fiona.open(region_shapefile, 'r') as region_shapes:
+        #for (band, lu_category) in land_use_categories:
+        #    zs = rasterstats.zonal_stats(region_shapefile, land_use_raster,
+        #                                      band = band, stats=['sum'])
+
+        zs = rasterstats.zonal_stats(region_shapefile, land_use_raster_single_band,
+                                            categorical=True, category_map=land_use_categories, geojson_out=True)
+
+        print zs[1]
+
+        schema = {'geometry': 'Polygon',
+                'properties': [ ('AGS_Int', 'int'), ('GEN', 'str'), ('Area', 'float')]}
+
+        land_use_properties = zip(zip(*land_use_categories)[1], repeat('int'))
+        print land_use_properties
+        schema['properties'].extend(land_use_properties)
+
+        print schema
+
+        with fiona.open(
+         output_shapefile, 'w',
+         driver=region_shapes.driver,
+         crs=region_shapes.crs,
+         schema=schema) as c:
+                for region in zs:
+                    old_properties = region['properties']
+
+                    new_properties = {
+                        'GEN': old_properties['GEN'],
+                        'Area': old_properties['Shape_Area'],
+                        'AGS_Int': old_properties['AGS_Int'],
+                    }
+
+
+                    total = 0
+                    for (k,cat_name) in land_use_categories:
+                        if (k, cat_name) in old_properties and k != 0:
+                           value = old_properties[(k,cat_name)] * 100
+                           new_properties[cat_name] = value
+                           total += value
+                        else:
+                            new_properties[cat_name] = 0
+
+                    new_properties['Remainder'] = new_properties['Area_calc'] - total
+
+
+                    region['properties'] = new_properties
+
+                    c.write(region)
+
+
+
+Config = ConfigParser.ConfigParser(allow_no_value=True)
+
+if len(sys.argv) == 1 or not os.path.exists(sys.argv[1]):
+    raise IOError("please supply a configuration file as a program arugment")
+Config.read(sys.argv[1])
+
+cmap = [(float(k), v) for k,v in
+            [tuple(Config.get("Class Values", c).split(',')) for c in Config.options("Class Values")]
+        ]
+
+print cmap
+
+calculate_region_land_use("../../data/regional/Auspendler_in_Kernregion_25Proz_geglaettet",
+                          "../../output/regions_with_land_use",
+                          "../../data/land_use/land_use_merged_10m.tif", cmap)
 
 
 def calculate_base_cell_disaggregation_factor(base_cells, regions, land_use_features):
