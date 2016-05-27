2f573ac21fd5826a17fa5bf1396de492baa96013
Joe Molloy <joe.m34@gmail.com>
2016-05-27 10:05:09 +0200

    update the land use aggregator to make it a bit more flexible, will accept various compression ratios now

--- code/raster_file_aggregation.py
+++ code/raster_file_aggregation.py
@@ -14,20 +14,33 @@
 
 # create a 100 x 100m raster of land use, 5 bands with sqM coverage, 1 band of region
 # build using a numpy array, and same affine base as population
+
+#(resolution of output/resolution of input) must be less than 16 + i.e. 100/10 = 10.
 import numpy as np
 import rasterio
 from affine import Affine
 
-size = 2048
 #max square area at 100m resolution is 100:
-def disaggregate(m10_data):
+def disaggregate(m10_data, ratio, bands):
+    assert(isinstance(ratio, int))
+
     (height, width) = m10_data.shape
-    new_array_height = height/10 + 1
-    new_array_width = width/10 + 1
+    new_array_height = height/ratio + 1
+    new_array_width = width/ratio + 1
     print "old array: ", (height, width)
     print "new array: " , (new_array_height, new_array_width)
 
-    land_use_array = np.zeros((6,new_array_height, new_array_width), dtype=np.ubyte)
+    if ratio < pow(2,8):
+        output_data_type = rasterio.ubyte
+    elif ratio < pow(2,16):
+        output_data_type = rasterio.uint16
+    elif ratio < pow(2,32):
+        output_data_type = rasterio.uint32
+    else:
+        raise Exception("compression ratio is too large, please select a smaller output resolution")
+
+
+    land_use_array = np.zeros((bands,new_array_height, new_array_width), dtype=output_data_type)
 
     #start here for values : 76, 8892
     start_x = 0
@@ -35,23 +48,26 @@ def disaggregate(m10_data):
     end_x = width
     end_y = height
 
-    for y in xrange(start_y, end_y, 10):
-        for x in xrange(start_x, end_x, 10):
-            window = m10_data[y:y+10, x:x+10]
+    for y in xrange(start_y, end_y, ratio):
+        for x in xrange(start_x, end_x, ratio):
+            window = m10_data[y:y+ratio, x:x+ratio]
             (w_height, w_width) = window.shape
             if w_width * w_height > 0:
 
-                bin_counts = np.bincount(window.ravel(), minlength=6)
+                bin_counts = np.bincount(window.ravel(), minlength=bands)
                 #if bin_counts[0] < 100: print bin_counts
 
-                for i in xrange(0,5):
-                    land_use_array[i, y/10, x/10] = bin_counts[i]
-
-    print land_use_array[:,7:77, 889:899]
+                for i in xrange(0,bands-1):
+                    land_use_array[i, y/ratio, x/ratio] = bin_counts[i]
 
     return land_use_array
 
-with rasterio.open("../land_use_merged.tif", 'r') as land_use_raster:
+output_resolution = 100
+bands = 6
+input_file = "../land_use_merged.tif"
+output_file = "../land_use_100m.tif"
+
+with rasterio.open(input_file, 'r') as land_use_raster:
 
     affine_fine = land_use_raster.profile['affine']
     height = land_use_raster.profile['height']
@@ -67,27 +83,26 @@ with rasterio.open("../land_use_merged.tif", 'r') as land_use_raster:
     #     it.iternext()
 
     print affine_fine
+    ratio = int(output_resolution / affine_fine.a)
 
-    affine_gross = affine_fine * Affine.scale(10.0)
+    affine_gross = affine_fine * Affine.scale(ratio)
     print affine_gross
 
 
-    #(bands, new_array_height, new_array_width) = (1, 2136, 1980)
+    land_use_array = disaggregate(m10_data, ratio, bands)
+    (num_bands, new_array_height, new_array_width) = land_use_array.shape
 
-    land_use_array = disaggregate(m10_data)
-    (bands, new_array_height, new_array_width) = land_use_array.shape
-
-    profile.update(dtype=rasterio.ubyte,
-                   count=bands,
+    profile.update(dtype=land_use_array.dtype,
+                   count=num_bands,
                    transform=affine_gross,
                    nodata = 0,
                    height = new_array_height,
                    width = new_array_width)
 
-    with rasterio.open("../land_use_100m.tif", 'w', **profile) as out:
+    with rasterio.open(output_file, 'w', **profile) as out:
         print land_use_array.shape , "->", (out.height, out.width)
-        for k in xrange(0,bands):
-            out.write(land_use_array[k].astype(rasterio.uint8), indexes=k+1)
+        for k in xrange(0,num_bands):
+            out.write(land_use_array[k], indexes=k+1)
 
 
         #a = np.ones((new_array_height, new_array_width), dtype=rasterio.ubyte) * 127
