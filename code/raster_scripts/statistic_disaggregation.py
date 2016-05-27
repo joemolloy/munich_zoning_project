6ca5477c73f3b6e108a0e232df2a3719f2bad7d7
Joe Molloy <joe.m34@gmail.com>
2016-05-27 20:05:29 +0200

    improve attribute output of region land-use assignment (use percentages), initial work on calculating raster land use percentages

--- code/raster_scripts/statistic_disaggregation.py
+++ code/raster_scripts/statistic_disaggregation.py
@@ -17,6 +17,7 @@ import ConfigParser
 import sys, os
 from itertools import repeat
 from shapely.geometry import shape
+import numpy as np
 
 #CASE WHEN OBJART  = 'AX_Wohnbauflaeche' THEN 1
 #	WHEN OBJART  =  'AX_FlaecheBesondererFunktionalerPraegung' THEN 2
@@ -45,9 +46,10 @@ def calculate_region_land_use(region_shapefile, output_shapefile, land_use_raste
         print zs[1]
 
         schema = {'geometry': 'Polygon',
-                'properties': [ ('AGS_Int', 'int'), ('GEN', 'str'), ('Area', 'float')]}
+                'properties': [ ('AGS_Int', 'int'), ('GEN', 'str'), ('Area', 'float'), ('Area_covered', 'float')]}
 
-        land_use_properties = zip(zip(*land_use_categories)[1], repeat('int'))
+        #create land use properties for new schema (will be percentages)
+        land_use_properties = zip(zip(*land_use_categories)[1], repeat('float'))
         print land_use_properties
         schema['properties'].extend(land_use_properties)
 
@@ -67,45 +69,90 @@ def calculate_region_land_use(region_shapefile, output_shapefile, land_use_raste
                         'AGS_Int': old_properties['AGS_Int'],
                     }
 
-
-                    total = 0
+                    total_land_used = sum([old_properties[key]
+                                           for key in land_use_categories
+                                           if key[0] != 0 and key in old_properties])
+                    print total_land_used
                     for (k,cat_name) in land_use_categories:
                         if (k, cat_name) in old_properties and k != 0:
-                           value = old_properties[(k,cat_name)] * 100
-                           new_properties[cat_name] = value
-                           total += value
+                            value = float(old_properties[(k,cat_name)]) / total_land_used
+                            new_properties[cat_name] = value
+                            print "\t", old_properties[(k,cat_name)] ,"\t", value
                         else:
                             new_properties[cat_name] = 0
 
-                    new_properties['Remainder'] = new_properties['Area_calc'] - total
-
+                    new_properties['Area_covered'] = total_land_used*100
+                    new_properties['Remainder'] = new_properties['Area'] - new_properties['Area_covered']
 
                     region['properties'] = new_properties
 
                     c.write(region)
 
 
+#given a land_use array + transformation, mapping from region to land-uses and values, bounds of new array,
 
-Config = ConfigParser.ConfigParser(allow_no_value=True)
-
-if len(sys.argv) == 1 or not os.path.exists(sys.argv[1]):
-    raise IOError("please supply a configuration file as a program arugment")
-Config.read(sys.argv[1])
-
-cmap = [(float(k), v) for k,v in
-            [tuple(Config.get("Class Values", c).split(',')) for c in Config.options("Class Values")]
-        ]
-
-print cmap
-
-calculate_region_land_use("../../data/regional/Auspendler_in_Kernregion_25Proz_geglaettet",
-                          "../../output/regions_with_land_use",
-                          "../../data/land_use/land_use_merged_10m.tif", cmap)
+# {region : {key : value} }
+import math
+import rasterio
+def distribute_region_statistics(land_use_raster_file, region_raster_file, region_shapefile):
+    with rasterio.open(land_use_raster_file, 'r') as land_use_raster:
+        with rasterio.open(region_raster_file, 'r') as region_raster:
+            with fiona.open(region_shapefile, 'r'):
 
 
-def calculate_base_cell_disaggregation_factor(base_cells, regions, land_use_features):
-    pass
+                affine_fine = land_use_raster.profile['affine']
+                height = land_use_raster.profile['height']
+                width = land_use_raster.profile['width']
+                profile = land_use_raster.profile
 
+                lu_array = land_use_raster.read()
+
+                #move the bands to the third dimension to make calculations easier to read
+                lu_array_bands_last = np.rollaxis(lu_array, 0, 3)
+                print lu_array_bands_last.shape
+
+                #remove remaining column
+                REMAINDER_COL = 0
+                lu_without_remainder = np.delete(lu_array_bands_last,REMAINDER_COL, axis=2)
+                #expand summed area value so that the divison against each band will work
+                lu_agg = lu_without_remainder.astype(float) / np.atleast_3d(np.sum(lu_without_remainder, axis=2))
+
+                for (y,x,z), v in np.ndenumerate(lu_agg):
+                    total_cell_land_use = v
+                    if not math.isnan(v):
+                        print (y,x), z, total_cell_land_use
+
+
+
+
+
+
+
+#distribute_region_statistics("../../output/land_use_100m.tif", None)
+
+
+
+
+def run_calculate_region_land_use():
+
+    Config = ConfigParser.ConfigParser(allow_no_value=True)
+
+    if len(sys.argv) == 1 or not os.path.exists(sys.argv[1]):
+        raise IOError("please supply a configuration file as a program arugment")
+    Config.read(sys.argv[1])
+
+    cmap = [(float(k), v) for k,v in
+                [tuple(Config.get("Class Values", c).split(',')) for c in Config.options("Class Values")]
+            ]
+
+    print cmap
+
+    calculate_region_land_use("../../data/regional/Auspendler_in_Kernregion_25Proz_geglaettet",
+                              "../../output/regions_with_land_use",
+                              "../../data/land_use/land_use_merged_10m.tif", cmap)
+
+
+run_calculate_region_land_use()
 
 
 #
